e9c25b9de56c14f138607c9fdc75f3f97344d7f8
gruand <gruand@gmail.com>
2024-03-27 18:06:33 +0200

    Add url pattern for contact us page

--- polls/urls.py
+++ polls/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("contacts/", views.contacts, name="contacts"),
 ]
