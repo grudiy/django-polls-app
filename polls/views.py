ab48b50ddc89c5e1d8725d635ef7990b0e5e8af8
gruand <gruand@gmail.com>
2024-03-27 17:40:36 +0200

    Fix template path

--- polls/views.py
+++ polls/views.py
@@ -7,7 +7,7 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+    return render(request, "index.html", context)
 
 
 def detail(request, question_id):
