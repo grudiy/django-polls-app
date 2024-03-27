e4a171d61aae100d419fe9c9687096958ff8d723
gruand <gruand@gmail.com>
2024-03-27 18:07:26 +0200

    Add view and html for detail page

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Question
 
 
@@ -11,7 +11,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "detail.html", {"question": question})
 
 
 def results(request, question_id):
