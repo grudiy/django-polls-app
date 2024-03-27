6c0c4fb05a0c99cca0ece07a7ab7406867f5b407
gruand <gruand@gmail.com>
2024-03-27 17:55:55 +0200

    Add contact us view and html template

--- polls/views.py
+++ polls/views.py
@@ -21,3 +21,7 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+def contacts(request):
+    return render(request, "contacts.html")
