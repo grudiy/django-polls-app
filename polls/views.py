7a7986e20f2d251e805106c204efcb06e6048d8e
gruand <gruand@gmail.com>
2024-03-27 18:16:46 +0200

    Rewrite detail view to shortcut: get_object_or_404()

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Question
 
@@ -11,10 +11,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "detail.html", {"question": question})
 
 
